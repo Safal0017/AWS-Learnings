91251e30c174513cece23f8346ea339ca8820fcb
Safal0017 <safalpatelsjp203039@gmail.com>
2024-03-25 21:06:07 -0400

    Testing CI workflow after deleting CD services!!

--- CICD/day-14/simple-python-app/app.py
+++ CICD/day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, safal, Testing CICD Workflow, count-4!!'
+    return 'Hello, safal, Testing CI Workflow, deleted CD services!!'
 
 if __name__ == '__main__':
     app.run()
