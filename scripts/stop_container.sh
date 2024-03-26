017594c1062b70e5a185dcdc28ea151e05dc8405
Safal Patel <68127666+Safal0017@users.noreply.github.com>
2024-03-26 01:35:56 -0400

    Updated stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,10 +2,15 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hello, changes revoked"
+# echo "Hello, changes revoked"
 
 # echo "Container deletion initiated!!"
 # containerid=`docker ps | awk -f "" '{print $1}'`
 # docker rm -f $containerid
 # echo "Container deleted successfully!!"
+
+echo "Container deletion initiated!!"
+container_id=$(sudo docker ps --format "{{.ID}}")
+docker rm -f $container_id
+echo "Container deleted successfully!!"
 
